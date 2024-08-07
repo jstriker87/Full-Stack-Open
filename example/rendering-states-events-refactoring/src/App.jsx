7bcf2ca417fffa861668133df07d34feab113c5a
James Bart <jbart1@gmail.com>
2024-08-08 00:31:15 +0100

    Finished rendering, status events, and refactoring examples

--- example/rendering-states-events-refactoring/src/App.jsx
+++ example/rendering-states-events-refactoring/src/App.jsx
@@ -1,26 +1,31 @@
 import { useState } from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
-
-  const handleClick =() =>{
-    console.log("Clicked")
-
-  }
+  const increaseByOne = () => setCounter(counter +1)
+  const decreaseByOne = () => setCounter(counter -1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-    <div>{counter}</div>
-    <button onClick={()=>setCounter(counter +1)}>
-    Plus
-    </button>
-    <button onClick={()=>setCounter(0)}>
-    Reset
-    </button>
-
+    <Display counter={counter}/>
+    <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+    <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    <Button
+        onClick={setToZero}
+        text='reset'
+      />
     </div>
   )
 }
 
+const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
 export default App
