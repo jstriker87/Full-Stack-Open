c1c0a838e64024ffa5e61878a40d69c3a9f831ec
James Bart <jbart1@gmail.com>
2024-08-08 12:06:31 +0100

    Working on async example

---
+++ examples/async-states/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+
+const History = (props) => {
+    
+    if (props.allClicks.length === 0) {
+    <div>
+        the app is used by pressing the buttons
+      </div> 
+    }
+}
+    return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+
+const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+    </div>
+  )
+}
+export default App
