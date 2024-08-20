2ab71a46871dbe217a9f7ed37dde0703bcb034c8
James Bart <jbart1@gmail.com>
2024-08-20 14:23:41 +0100

    Completed exercise 2.9

--- part2/2.9/src/App.jsx
+++ part2/2.9/src/App.jsx
@@ -5,7 +5,7 @@ const [persons, setPersons] = useState([
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filterName, setFilterName] = useState(true)
+  const [filterName, setFilterName] = useState('')
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
@@ -17,12 +17,12 @@ const [persons, setPersons] = useState([
   if (persons.some(e => e.name === personObject.name)) {
     window.alert(`${newName} is already added to phonebook`)
     setNewName('')
-    exit(-1)
+    return -1
   }
   if (personObject.name.length === 0 || personObject.number.length === 0) {
     window.alert(`Name or number has not not been entered`)
     setNewName('')
-    exit(-1)
+    return -1
   }
 
         setNewName('')
@@ -36,10 +36,14 @@ const [persons, setPersons] = useState([
  const numberChange = (event) => {
     setNewNumber(event.target.value)
     }
-
+ const filterChange = (event) => {
+    setFilterName(event.target.value)
+    }
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with : <input value={filterName} onChange={filterChange}/>
       <form onSubmit={addPerson}>
         <div>
           <h2> Add a new </h2>
@@ -52,10 +56,14 @@ const [persons, setPersons] = useState([
       </form>
       <h2>Numbers</h2>
     <ul>
-        {persons.map(person => (
-            <li key={person.name}>{person.name} {person.number}</li>
+        {persons
+            .filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+            .map(person => (
+                <p key={person.name}>
+                    {person.name} {person.number}
+                </p>
         ))}
-    </ul>
+</ul>
     </div>
   )
 }
